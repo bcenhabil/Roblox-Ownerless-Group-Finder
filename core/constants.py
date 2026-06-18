2490308f30eab8175f1502b760f280ceaa76b8ec
bcenhabil <109930801+bcenhabil@users.noreply.github.com>
2026-06-18 16:45:03 +0800

    Create constants.py

---
+++ core/constants.py
@@ -0,0 +1,21 @@
+EMBED_FOOTER_TEXT = "github.com/yourusername/roblox-ownerless-finder"
+
+DEFAULT_RANGES = (
+    (1, 2_000_000),
+    (5_000_000, 15_000_000),
+    (20_000_000, 50_000_000),
+    (80_000_000, 120_000_000),
+    (150_000_000, 180_000_000),
+)
+
+PROXY_SOURCES = [
+    "https://cdn.jsdelivr.net/gh/proxyscrape/free-proxy-list@main/proxies/all/data.txt",
+    "https://raw.githubusercontent.com/mohammedcha/ProxRipper/main/full_proxies/http.txt",
+    "https://raw.githubusercontent.com/Thordata/awesome-free-proxy-list/main/proxies/all.txt",
+    "https://raw.githubusercontent.com/komutan234/Proxy-List-Free/main/proxies/http.txt",
+    "https://raw.githubusercontent.com/Ian-Lusule/Proxies/main/proxies/all_proxies.txt",
+    "https://raw.githubusercontent.com/proxygenerator1/ProxyGenerator/main/MostStable/http.txt",
+    "https://raw.githubusercontent.com/iplocate/free-proxy-list/main/all-proxies.txt",
+]
+
+PROXY_REFRESH_INTERVAL = 300
